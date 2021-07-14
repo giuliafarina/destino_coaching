0b3b96687d44dbb74db80002b2dc97bcf0c7f49a
= <=>
2021-07-14 11:32:24 +0200

    modify text padding services

--- src/components/HeroSection/hero.js
+++ src/components/HeroSection/hero.js
@@ -7,10 +7,10 @@ const Hero = () => {
             <img src={example} />
         </div>
         <div className="quote"><blockquote>
-            <p>“But I don’t want comfort. <p>I want God, I want poetry, I want real danger, I want freedom, I want goodness. I want sin. </p></p></blockquote>
+            <p>“But I don’t want comfort. <p>I want God, I want poetry, I want real danger, I want freedom, I want goodness. I want sin." </p></p></blockquote>
             <figcaption>—Aldous Huxley, <cite>Brave New World</cite></figcaption>
         </div>
-        <div><p>
+        <div className="description"><p>
             Billions upon billions how far away venture rogue shores of the cosmic ocean white dwarf. Are creatures of the cosmos adipisci velit Euclid a mote of dust suspended in a sunbeam courage of our questions extraordinary claims require extraordinary evidence. Not a sunrise but a galaxyrise a mote of dust suspended in a sunbeam concept of the number one Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur another world Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
         </p></div>
 
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,9 @@
 import * as React from "react";
 import Layout from "../components/layout";
 
+
 const Services = () => {
-    return <Layout><header>What I do</header><div><p>Explorations two ghostly white figures in coveralls and helmets are softly dancing concept of the number one decipherment the ash of stellar alchemy billions upon billions. Vastness is bearable only through love across the centuries cosmic fugue something incredible is waiting to be known from which we spring permanence of the stars. Network of wormholes a mote of dust suspended in a sunbeam finite but unbounded hearts of the stars a very small stage in a vast cosmic arena how far away and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p></div></Layout>
+    return <Layout><header>What I do</header><div className="services-description"><p>Explorations two ghostly white figures in coveralls and helmets are softly dancing concept of the number one decipherment the ash of stellar alchemy billions upon billions. Vastness is bearable only through love across the centuries cosmic fugue something incredible is waiting to be known from which we spring permanence of the stars. Network of wormholes a mote of dust suspended in a sunbeam finite but unbounded hearts of the stars a very small stage in a vast cosmic arena how far away and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p></div></Layout>
 }
 
 export default Services;
