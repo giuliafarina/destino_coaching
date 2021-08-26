da6e8b23b733f8cd9b9fa4ad4ec927be8d2a5f30
= <=>
2021-08-26 14:08:18 +0200

    update about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,18 +5,46 @@ import * as styles from '../styles/about.module.css';
 
 const About = () => {
     return <Layout>
-        <div className={styles.about_container} ><h1>About Me</h1>
+        <h1 className={styles.title}>The DESTINO story</h1>
+        <img className={styles.about_img} src={nadia}></img>
 
-            <p>Hi, I am Nadia. I grew up in London with my Mum and brother and moved to East Anglia to take up Headship in 2015. I now love life in the countryside and live here with my partner and two children
-                who are very annoying but also bring me great joy!</p>
-            <p>I worked in London schools for 15 years before moving East. The school I moved here to lead, was recognised as being in the bottom 2% of schools for academic performance when I arrived, and the challenged that faced me, was greater than I ever imagined. I realised quite quickly that the only way to lead in such circumstances was to be the best person I possibly could be, so I embarked on a counselling training course and started my journey to becoming a  coach.
-                Coaching became a way of being in my school.</p><img className={styles.about_img} src={nadia}></img>
-            <p>My deputy had this to say:
-                Working with Nadia is hugely rewarding - she listens attentively and asks the questions that prompt you to think and reflect on ways to move forward. Her warm and vibrant personality contributes to coaching sessions that I enjoy and look forward to. She has helped me over the past few years to find myself and be the best version of me that I can be.
-                (Stephany hunter, Headteavher Barningham Primary School Suffolk).</p>
-            <p>Since leaving headship, I have been coaching school leaders and providing support to  schools that want to develop coaching approaches. I also  work with Achievement For All, delivering their Achieving Schools and Achieving Wellbeing programmes, as well as Ambition School Leadership and Inspiration Trust, facilitating on their NPQs. I wholeheartedly believe coaching approaches will retain teachers,
-                strengthen school leadership and serve our children and young people.</p>
-            <p>When I am not working, I enjoy walking, cycling (gently on a granny bike) and I have always found comfort in fabric and crafts (currently it’s crochet).</p><p>My ultimate goal is to influence educational policy through empowering school leaders and I would love you to help me achieve this.</p>
+        <div className={styles.description}>
+            <p className={styles.greeting}>Hi, I am <b>Nadia</b>,
+                and I am the face behind <b>Destino Coaching.</b>
+            </p>
+            <br />
+            <h1 className={styles.title}>About me</h1>
+            <p> I grew up in London with my Mum and brother
+                and moved to East Anglia to take up Headship in 2015.
+            </p>
+            <br />
+            <p>I now love life in the countryside and live here with my partner and two children
+                who are very annoying but also bring me great joy!
+            </p>
+            <br />
+            <p>When I am not working, I enjoy walking, cycling (gently on a granny bike)
+                and I have always found comfort in fabric and crafts (currently it’s crochet).
+            </p>
+            <br />
+        </div>
+
+        <div className={`${styles.description} ${styles.container_pink}`}>
+            <h1 className={styles.title}>The birth of Destino</h1>
+            <p>I worked in London schools for 15 years before moving East.</p>
+            <p>The school I moved here to lead, was recognised as being in the bottom 2% of schools for academic performance when I arrived,
+                and the challenged that faced me, was greater than I ever imagined.</p>
+            <br />
+            <p>I realised quite quickly that the only way to lead in such circumstances was to be <b>the best person I possibly could be</b>, so I embarked on a <b>counselling training course</b> and started my journey to becoming a  coach.</p>
+            <br />
+            <h3><b>Coaching became a way of being in my school</b>.</h3>
+        </div>
+
+        <div className={`${styles.description} ${styles.container_green}`}>
+            <p>Since leaving headship, I have been coaching school leaders and providing <b>support to  schools that want to develop coaching approaches</b>.</p>
+            <br /><p> I also  work with <b>Achievement For All</b>, delivering their <b>Achieving Schools</b> and <b>Achieving Wellbeing</b> programmes, as well as <b>Ambition School Leadership and Inspiration Trust</b>, facilitating on their <b>NPQs</b>.</p><br />
+            <p> I wholeheartedly believe coaching approaches will retain teachers,
+                strengthen school leadership and serve our children and young people.</p><br />
+            <p>My ultimate goal is to influence educational policy through empowering school leaders and I would love you to help me achieve this.</p>
         </div>
     </Layout>
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -10,19 +10,22 @@ import Card from "../components/card";
 
 
 const Services = () => {
-    return <Layout><h1 className={styles.title}>WHAT I DO</h1><div><img className={styles.coaching_img} src={coaching} /></div><div className={styles.description}>
-        <p>
-            I am a certified <b>life and leadership coach</b> and I work with clients in a few different ways.</p><br />
-        <p>In all my coaching work, I challenge clients to stretch their capacity by asking for far more than they
-            have done before.</p><br /><p> I use a <b>non-judgemental approach</b> while being open and honest about what I hear and see.</p><br /><p>
-            <b>Coaching </b>is confidential, developmental, safe and provokes the client into action.
-            I am a warm and sincere coach and take pride in making sure my clients feel their goal has been achieved through coaching.
-            I keep in touch with clients between sessions and continually improve my
-            coaching skills through my own ongoing professional development.</p></div>
+    return <Layout><h1 className={styles.title}>WHAT I DO</h1>
+        <div><img className={styles.coaching_img} src={coaching} /></div>
+        <div className={styles.description}>
+            <p>
+                I am a certified <b>life and leadership coach</b> and I work with clients in a few different ways.</p><br />
+            <p>In all my coaching work, I challenge clients to stretch their capacity by asking for far more than they
+                have done before.</p><br /><p> I use a <b>non-judgemental approach</b> while being open and honest about what I hear and see.</p><br /><p>
+                <b>Coaching </b>is confidential, developmental, safe and provokes the client into action.
+                I am a warm and sincere coach and take pride in making sure my clients feel their goal has been achieved through coaching.
+                I keep in touch with clients between sessions and continually improve my
+                coaching skills through my own ongoing professional development.</p>
+        </div>
 
         <div className="container_section_green">
 
-            <div className={styles.description}>   <h1>Coaching Services</h1><p>1. Leadership coaching
+            <div className={styles.description_section_green}>   <h1>Coaching Services</h1><p>1. Leadership coaching
                 Leadership coaching identifies leadership strengths and areas of improvement for an individual or a team. It focuses on developing  emotional intelligence and defining a future vision for oneself as a leader.
                 2. Executive coaching
                 Executive coaching is for leaders and improves  leadership abilities as well as the challenges associated with managing organisational change. This is ideal for Headteachers,, executive leaders and CEOs.
