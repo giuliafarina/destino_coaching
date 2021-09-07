fda18bfade2038d2817aa286b83342cc645a7a5a
= <=>
2021-09-07 08:35:22 +0200

    add picture ServicesPrev

--- src/components/ServicesPrev.js
+++ src/components/ServicesPrev.js
@@ -3,10 +3,12 @@ import * as React from "react";
 import * as styles from "../styles/servicesPrev.module.css"
 import Button from "./button.js";
 import { Link } from "gatsby";
+import desk from "../images/desk.jpeg";
 
 const ServicesPrev = ({ idProp }) => {
     return <>
         <div className="container_section_pink" id={idProp}>
+            <img className={styles.desk_image} src={desk} />
             <div className={styles.container_content_pink}>
                 <div className={styles.content_pink}><h1 className={styles.title_pink}>What I do</h1>
                     <p> I am a certified life and leadership coach and I work with clients in a few different ways. In all my coaching work,
