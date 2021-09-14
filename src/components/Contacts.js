561cea13634403ef1ea8bbafbd0b9e854af44fbf
= <=>
2021-09-14 13:36:48 +0200

    insert icons footer

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,18 +1,23 @@
 import * as React from "react";
+import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
+import { IoLogoFacebook, IoLogoLinkedin } from "react-icons/io5"
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import InstagramIcon from '@material-ui/icons/Instagram';
+// import { Facebook } from "@material-ui/icons";
 
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import MailIcon from '@material-ui/icons/Mail';
+
 
 const Contacts = ({ idProp }) => {
     return (
 
-        <footer className="social"><p>Feel free to get in touch:</p>
-
-            <p id={idProp}><a href="mailto:paperino@gmail.com" ><MailIcon /></a><a><TwitterIcon /></a><a><LinkedInIcon /></a><InstagramIcon /><a><FacebookIcon /></a></p>
-
+        <footer >
+            <p className="social" id={idProp}>
+                <a className="instaIcon">  <AiFillInstagram /></a>
+                <a className="linkedinIcon"><IoLogoLinkedin /></a>
+                <a className="twitterIcon"><AiFillTwitterCircle /></a>
+                <a className="facebookIcon"><IoLogoFacebook /></a></p>
+            <p className="credits">© Destino Coaching 2021<br /> Web Design: Virginia Broadhead & Giulia Farina <br />Photo credits: Emma Cullen</p>
         </footer>
     )
 }
