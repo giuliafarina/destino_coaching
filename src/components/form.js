1dc37fa7df4285e8610e153b679ee89a82435662
= <=>
2021-09-16 10:41:01 +0200

    finish desktop css for Form component

--- src/components/form.js
+++ src/components/form.js
@@ -1,21 +1,23 @@
 import * as React from "react";
+import * as styles from "../styles/form.module.css"
 
 const Form = () => {
 
     return <>
-        <h1>Let's chat!</h1>
-        <p>Please contact me if you would like to book a consultation call.
-            Use this form to send me a message and we'll arrange a meeting.</p>
-        <form action="emailto:nadia_destino@outlook.com">
-            <label for="fname">Name</label><br />
-            <input type="text" id="fname" name="fname" placeholder="Type in your name"></input><br />
-            <label for="email">Email</label><br />
-            <input type="email" id="email" name="email" placeholder="Type in your email"></input><br />
-            <label for="message">Message</label><br />
-            <textarea id="message" name="message" placeholder="Type in your message"></textarea><br />
-            <input type="submit" value="Submit" />
+        <div className={styles.form_container}>
+            <h1>Let's chat!</h1><br />
+            <p>Please contact me if you would like to book a consultation call.
+                Use this form to send me a message and we'll arrange a meeting.</p><br />
+            <form action="emailto:nadia_destino@outlook.com">
+                <label for="fname">Name</label><br />
+                <input type="text" id="fname" name="fname" placeholder="Type in your name"></input><br />
+                <label for="email">Email</label><br />
+                <input type="email" id="email" name="email" placeholder="Type in your email"></input><br />
+                <label for="message">Message</label><br />
+                <textarea id="message" name="message" placeholder="Type in your message"></textarea><br />
+                <input className={styles.submit_button} type="submit" value="Submit" />
 
-        </form>
+            </form></div>
     </>
 
 }
