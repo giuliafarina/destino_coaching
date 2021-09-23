98aea099cd21053db93e046ef4a2c57c42159638
= <=>
2021-09-23 11:04:54 +0200

    change text white section

--- src/components/whiteSection.js
+++ src/components/whiteSection.js
@@ -16,7 +16,7 @@ const WhiteSection = () => {
                     <p>I grew up in North London and moved to East Anglia to take up Headship in 2015.
                         I now love life in the countryside and live here with my partner and two teenage children.</p><br />
                     <p>I worked in London schools for 15 years before moving East. The school I moved here to lead, was recognised as being in the bottom 2% of schools for academic performance when I arrived,
-                        and the challenged that faced me, was greater than I ever imagined.</p>
+                        and the challenge that faced me, was greater than I ever imagined.</p>
                     <br />
                     <p>I realised quite quickly that the only way to lead in such circumstances was to be <b>the best person I possibly could be</b>, so I embarked on a <b>counselling training course</b> and started my journey to becoming a  coach.
 
