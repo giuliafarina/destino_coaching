069d5ea17426feed340b9d4ef36221c5f604fc24
= <=>
2021-09-30 20:04:19 +0200

    Add missing return statement

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -2,9 +2,9 @@ import * as React from "react";
 import Layout from "../components/layout";
 
 const ThankYouPage = () => {
-    <Layout>
+    return <Layout>
         <p>Thank you for your message!</p>
     </Layout>
-};
+}
 
 export default ThankYouPage;
