3e259113d6c8d8548440d6a96e49677fd109b598
= <=>
2021-09-30 12:46:02 +0200

    add thankyou page

---
+++ src/pages/ThankYou.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const ThankYouPage = () => {
+    <Layout>
+        <p>Thank you for your message!</p>
+    </Layout>
+};
+
+export default ThankYouPage;
