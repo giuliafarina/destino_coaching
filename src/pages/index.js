bd49cf6384fafd57c667d7b4206f6572688add2f
= <=>
2021-12-01 11:14:10 +0100

    add google analytics gatsby  plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,27 +12,14 @@ const IndexPage = () => {
   return (
     <React.Fragment>
       <Helmet>
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="author" content="Nadia Hewstone" />
-          <meta name="description" content="Destino Coaching" />
-          <meta property="og:title" content="Destino Coaching" />
-          <meta property="og:description" content="Coaching Services" />
-          <meta property="og:site_name" content="destinocoaching.co.uk" />
-          <title>Destino Coaching</title>
-          <link rel="icon" type="image/svg+xml" href={favicon} />
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213980477-1"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){`dataLayer.push(arguments);`}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-213980477-1');
-          </script>
-
-        </head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Nadia Hewstone" />
+        <meta name="description" content="Destino Coaching" />
+        <meta property="og:title" content="Destino Coaching" />
+        <meta property="og:description" content="Coaching Services" />
+        <meta property="og:site_name" content="destinocoaching.co.uk" />
+        <title>Destino Coaching</title>
+        <link rel="icon" type="image/svg+xml" href={favicon} />
       </Helmet>
       <Layout>
         <Hero idProp="Hero" />
