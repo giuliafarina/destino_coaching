7dda2759edfd1b892ca42ee93635d3eaeb4ca183
Giulia Farina <giuliafarina88@gmail.com>
2021-12-20 14:20:25 +0100

    add ICF logo to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import { AiFillInstagram, AiFillTwitterSquare } from "react-icons/ai";
 import { IoLogoLinkedin } from "react-icons/io5"
 import { IoLogoFacebook } from "react-icons/io";
 import logoHT from "../images/logoHT.png"
+import logoICF from "../images/logoICF.png"
 
 
 
@@ -17,7 +18,7 @@ const Footer = () => {
                 <a className="twitterIcon" href="https://twitter.com/DestinoNadia"><AiFillTwitterSquare alt="twitter link" /></a>
                 <a className="facebookIcon" href="https://www.facebook.com/NadiaDestino"><IoLogoFacebook alt="facebook link" /></a></p>
             <div className="container_credits"><p className="credits"><p className="copyright"><b>© Destino Coaching 2021</b>
-                <img className="logoHT" src={logoHT} /></p>Design: <a href="https://www.linkedin.com/in/virginia-b-7188b15a">Virginia Broadhead</a>  <br />Coding: <a href="https://www.giuliafarina.com">Giulia Farina</a> <br />Photo credits: <a href="https://www.emmacullenphotography.com/">Emma Cullen</a></p></div>
+                <img className="logoHT" src={logoHT} /><img className="logoICF" src={ICFlogo} /></p>Design: <a href="https://www.linkedin.com/in/virginia-b-7188b15a">Virginia Broadhead</a>  <br />Coding: <a href="https://www.giuliafarina.com">Giulia Farina</a> <br />Photo credits: <a href="https://www.emmacullenphotography.com/">Emma Cullen</a></p></div>
         </footer>
     )
 }
